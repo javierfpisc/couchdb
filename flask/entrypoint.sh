2196b645caa898a3efaf2d1951cd24679bf176eb
José Pereira <info@jpereira.net>
2023-07-16 16:55:26 +0000

    cambios pereira

--- flask/entrypoint.sh
+++ flask/entrypoint.sh
@@ -7,7 +7,7 @@ cd /opt/web2print/website
 pip install --no-cache-dir -r requirements.txt
 
 #Levanta gunicorn
-gunicorn --bind 0.0.0.0:8000 --log-level=debug --error-logfile /opt/web2print/website/flask.log --access-logfile /opt/web2print/website/flask.log  website:app
+gunicorn --bind 0.0.0.0:8000 --timeout 600 --workers 3 --worker-class gevent --log-level=debug --error-logfile /opt/web2print/website/flask.log --access-logfile /opt/web2print/website/flask.log  website:app
 
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
